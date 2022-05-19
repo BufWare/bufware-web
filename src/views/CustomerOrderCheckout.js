2f4e6120a461320c27113bd76d92b23936b7aab6
Th0be <navrat.tobias@gmail.com>
2022-05-19 14:54:49 +0200

    Opraveno fetchovani seznamu objednávek pro zákazníky a chyba HTTP429

--- src/views/CustomerOrderCheckout.js
+++ src/views/CustomerOrderCheckout.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import ChosenOrders from  '../components/ChosenOrders'
+import {useEffect, useState} from 'react';
+import ChosenOrders from  '../components/ChosenOrders';
 import './CustomerOrderCheckout.css';
 
+
 function CustomerOrderCheckout() {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        getOrders();
+        const interval=setInterval(getOrders,1000);
+        return()=>clearInterval(interval);
+    }, []);
+
+    const getOrders = async () => {
+        try{
+            const response = await fetch("https://62849a953060bbd3473b9bce.mockapi.io/overview");
+            const orders = await response.json();
+            setOrders(orders);
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     return(
         <div className="orderList">
             <h1>Seznam objednávek</h1>
             <hr/>
             <h2>Objednané</h2>
             <div className="chosenOrderList">
-                <ChosenOrders type={0}/>
+                <ChosenOrders type={0} orders={orders}/>
             </div>
             <h2>Připravené</h2>
             <div className="chosenOrderList">
-                <ChosenOrders type={1}/>
+                <ChosenOrders type={1} orders={orders}/>
             </div>
         </div>
     );
