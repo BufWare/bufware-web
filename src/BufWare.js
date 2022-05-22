37058eafd4b3add008db406298f45178c3ba21ba
Matěj Pagáč <pagacmates@gmail.com>
2022-05-22 18:27:26 +0200

    Přidání tabulky produktů a vytvoření form na add

--- src/BufWare.js
+++ src/BufWare.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import CustomerOrderCheckout from './views/CustomerOrderCheckout';
+import CreateOrder from './views/CreateOrder';
+import AddProduct from './views/AddProduct';
 
 function BufWare (){
     return(
         <Routes>
-            <Route path="/" element={ <CustomerOrderCheckout/>}> //defaultní stránka
                 <Route path="customer-order-checkout" element={ <CustomerOrderCheckout/>}/>
-            </Route>
+                <Route path="create-order" element={ <CreateOrder/>}/>
+                <Route path="" element={<AddProduct/>} /> {/*defaultní stránka*/}
         </Routes>
     )
 };
