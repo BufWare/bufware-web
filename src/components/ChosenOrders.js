530bbb3ec9de941868960843c11220b151c51a96
Th0be <navrat.tobias@gmail.com>
2022-05-27 11:35:32 +0200

    Oprava zobrazování žádných objednávek u zákazníků

--- src/components/ChosenOrders.js
+++ src/components/ChosenOrders.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 export default function ChosenOrders({type, orders}){
-    return (
-        <div>
-            {
-                orders.filter(order => (order.stav===type)).map(filteredOrder => <div className="order" key={filteredOrder.id}>{filteredOrder.id}</div>)
-            }
-        </div>
-    )
+    if (orders.filter(order => (order.stav===type)).length>0){
+        return (
+            <>
+                {
+                    orders.filter(order => (order.stav===type)).map(filteredOrder => <div className="order" key={filteredOrder.id}>{filteredOrder.id}</div>)
+                }
+            </>
+        )
+    }
+    else{
+        return(
+            <div className='noOrders'>Žádné objednávky s tímto stavem</div>
+        )
+    }
 }
